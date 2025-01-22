e28405e4ce83de54ca3cb63fe61360791f4589ce
Wadje <toddnunes0@gmail.com>
2025-01-22 16:48:25 -0300

    1o commit

---
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -0,0 +1,11 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.ResponseBody; 
+@Controller 
+public class HomeController { 
+@RequestMapping("/Wadje") 
+public @ResponseBody String greeting() { 
+return "Hello, Wadje!"; 
+} 
+}
