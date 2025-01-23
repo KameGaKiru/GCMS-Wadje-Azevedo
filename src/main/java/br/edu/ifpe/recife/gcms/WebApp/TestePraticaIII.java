e0bf3f24eb88edf12f9cf109555a8fd07f3bf502
Wadje <toddnunes0@gmail.com>
2025-01-23 10:37:11 -0300

    atualização

--- src/main/java/br/edu/ifpe/recife/gcms/WebApp/TestePraticaIII.java
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/TestePraticaIII.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestePraticaIII {
 	
-	@RequestMapping("/Juncao") 
+	@RequestMapping("/Mudaca") 
 	public @ResponseBody String greeting() { 
-	return "fazer juncao de de clone e original"; 
+	return "mudança feita para juntar e dar push no GIT"; 
 	} 
 	//irei fazer um comentário e mudar aqui antes de fazer a juncao
 	}
